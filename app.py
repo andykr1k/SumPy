a66865423bff78fb240e17b7a8b04d2eae83af7a
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-06-12 21:41:12 -0700

    Added Footer

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-
 def main():
     load_dotenv()
     st.set_page_config(page_title="SumPy", page_icon="📄")
@@ -16,9 +15,54 @@ def main():
     hide_streamlit_style = """
             <style>
             MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
             </style>
             """
+    ft = """
+    <style>
+    a:link , a:visited{
+    color: #BFBFBF;  /* theme's text color hex code at 75 percent brightness*/
+    background-color: transparent;
+    text-decoration: none;
+    }
+
+    a:hover,  a:active {
+    color: #0283C3; /* theme's primary color*/
+    background-color: transparent;
+    text-decoration: underline;
+    }
+
+    #page-container {
+      position: relative;
+      min-height: 10vh;
+    }
+
+    footer{
+        visibility:hidden;
+    }
+
+    .footer {
+    position: fixed;
+    display: flex;
+    justify-items: center;
+    bottom: 0;
+    width: 100%;
+    background-color: transparent;
+    color: #808080; /* theme's text color hex code at 50 percent brightness*/
+    text-align: left; /* you can replace 'left' with 'center' or 'right' if you want*/
+    }
+    </style>
+
+    <div id="page-container">
+
+    <div class="footer">
+    <p style='font-size: 0.875em;'>Made with
+    with <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/>&nbsp;by <a style='display: inline; text-align: left;' href="https://github.com/andykr1k" target="_blank">Andrew Krikorian</a></p>
+    </div>
+
+    </div>
+    """
+    st.write(ft, unsafe_allow_html=True)
+
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
     # upload file
     pdf = st.file_uploader("Upload your PDF", type="pdf")
@@ -46,6 +90,10 @@ def main():
       # show user input
       user_question = st.text_input("Ask a question about your PDF:")
       if user_question:
+        loadState = st.empty()
+        answerState = st.empty()
+        loadState.text("Loading...")
+
         docs = knowledge_base.similarity_search(user_question)
         
         llm = OpenAI()
@@ -53,8 +101,10 @@ def main():
         with get_openai_callback() as cb:
           response = chain.run(input_documents=docs, question=user_question)
           print(cb)
-           
-        st.write(response)
+
+        loadState.empty()
+        
+        answerState.write(response)
     
 
 if __name__ == '__main__':
